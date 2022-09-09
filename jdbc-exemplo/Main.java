d4e783e1f2344b35e018bf0fa4f5d170b1fff39c
brunoahcor.mentoria <bruno.ahcor.mentoria@gmail.com>
2022-09-09 01:24:49 -0300

    Add listar registros

--- jdbc-exemplo/Main.java
+++ jdbc-exemplo/Main.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
@@ -16,23 +17,25 @@ public class Main {
     private static final String SQL_TABLE_DROP = "DROP TABLE PESSOA";
 
     private static final String SQL_TABLE_CREATE = "CREATE TABLE PESSOA"
-    + "("
-    + " ID serial,"
-    + " NOME varchar(100) NOT NULL,"
-    + " EMAIL varchar(50) NOT NULL,"
-    + " CREATED_DATE timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-    + " UPDATED_DATE timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-    + " PRIMARY KEY (ID)"
-    + ")";
+            + "("
+            + " ID serial,"
+            + " NOME varchar(100) NOT NULL,"
+            + " EMAIL varchar(50) NOT NULL,"
+            + " CREATED_DATE timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+            + " UPDATED_DATE timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+            + " PRIMARY KEY (ID)"
+            + ")";
 
     private static final String SQL_INSERT = "INSERT INTO PESSOA (NOME, EMAIL, CREATED_DATE, UPDATED_DATE) VALUES (?,?,?,?)";
 
     private static final String SQL_UPDATE = "UPDATE PESSOA SET EMAIL=?,UPDATED_DATE=? WHERE NOME=?";
 
+    private static final String SQL_SELECT = "SELECT * FROM PESSOA";
+
     public static void main(String[] args) {
 
         try (Connection conn = DriverManager.getConnection(URL.concat(DB), USER, PASS)) {
-            
+
             System.out.println("### Conectou ao banco postgres!");
 
             Statement statement = conn.createStatement();
@@ -41,9 +44,9 @@ public class Main {
 
             conn.setAutoCommit(false);
 
+            // Insere 2 registros
             PreparedStatement psInsert = conn.prepareStatement(SQL_INSERT);
-            
-            // Run list of insert commands
+
             psInsert.setString(1, "mateus");
             psInsert.setString(2, "mateus@email.com");
             psInsert.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
@@ -56,6 +59,7 @@ public class Main {
             psInsert.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
             psInsert.execute();
 
+            // Atualiza um registro
             PreparedStatement psUpdate = conn.prepareStatement(SQL_UPDATE);
 
             psUpdate.setString(1, "victor_victor@email.com");
@@ -63,10 +67,25 @@ public class Main {
             psUpdate.setString(3, "victor");
             psUpdate.execute();
 
+            // Lista todos os registros
+            PreparedStatement psSelect = conn.prepareStatement(SQL_SELECT);
+
+            ResultSet rs = psSelect.executeQuery();
+
+            while (rs.next()) {
+
+                System.out.print(rs.getLong("ID")+", ");
+                System.out.print(rs.getString("NOME")+", ");
+                System.out.print(rs.getString("EMAIL")+", ");
+                System.out.print(rs.getTimestamp("CREATED_DATE")+", ");
+                System.out.println(rs.getTimestamp("UPDATED_DATE"));
+
+            }
+
             conn.commit();
 
             conn.setAutoCommit(true);
-           
+
         } catch (SQLException se) {
             System.out.println("### Nao conectou!");
             System.out.println("SQLException: " + se.getMessage());
